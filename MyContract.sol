12d7ab12bd38c3794f1f4d2c0c3b800af99b7f48
Brandon Dionisio <brandon.dionisio.26@gmail.com>
2017-09-09 17:13:25 -0400

    Add initialSupply as a parameter to MyToken function rather than hardcoding 21000000

--- MyContract.sol
+++ MyContract.sol
@@ -5,10 +5,14 @@ contract MyToken {
   // 'mapping' = associate array where you associate addresses with balances
   // 'public' keyword means variable will be accessible by anyone in the blockchain
   // all balances will be public
+
   mapping (address => uint256) public balanceOf;
 
   // Create a few tokens on startup
-  function MyToken() {
-    balanceOf[msg.sender] = 21000000;
+  // Startup function that runs once the contract is uploaded to the network
+  // This sets the balance of msg.sender, the user who deployed the contract
+
+  function MyToken(uint256 initialSupply) {
+    balanceOf[msg.sender] = initialSupply;
   }
 }
