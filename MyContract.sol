150607daf98c3555da7bfaa286d10cf145612e56
Brandon Dionisio <brandon.dionisio.26@gmail.com>
2017-09-09 20:34:15 -0400

    Declare name, symbol and decimals variables. Modify constructor function to give values to variables.

--- MyContract.sol
+++ MyContract.sol
@@ -1,6 +1,12 @@
 pragma solidity ^0.4.0;
 
 contract MyToken {
+  // set up basic info about contract
+
+  string public name;
+  string public symbol;
+  uint8 public decimals;
+
   // Create an associative array with all balances
   // 'mapping' = associate array where you associate addresses with balances
   // 'public' keyword means variable will be accessible by anyone in the blockchain
@@ -9,11 +15,14 @@ contract MyToken {
   mapping (address => uint256) public balanceOf;
 
   // Create a few tokens on startup
-  // Startup function that runs once the contract is uploaded to the network
+  // Constructor function that runs once the contract is uploaded to the network
   // This sets the balance of msg.sender, the user who deployed the contract
 
-  function MyToken(uint256 initialSupply) {
-    balanceOf[msg.sender] = initialSupply;
+  function MyToken (uint256 initialSupply, string tokenName, string tokenSymbol, uint8 decimalUnits) {
+    balanceOf[msg.sender] = initialSupply; // give creator all initial tokens;
+    name = tokenName;                      // set the name for display purposes
+    symbol = tokenSymbol;                  // set the symbol for display purposes
+    decimals = decimalUnits;               // amount of decimals for display purposes
   }
 
   // Send coins
