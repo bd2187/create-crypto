83f5eb3089649060c9e8366fe655263e37d4f5b5
Brandon Dionisio <brandon.dionisio.26@gmail.com>
2017-09-09 20:21:12 -0400

    Add logic in transfer function to check if sender has adequate balance to send. Also check for overflows

--- MyContract.sol
+++ MyContract.sol
@@ -1,7 +1,7 @@
 pragma solidity ^0.4.0;
 
 contract MyToken {
-  // Create array with all balances
+  // Create an associative array with all balances
   // 'mapping' = associate array where you associate addresses with balances
   // 'public' keyword means variable will be accessible by anyone in the blockchain
   // all balances will be public
@@ -17,7 +17,10 @@ contract MyToken {
   }
 
   // Send coins
-  function transfer(address _to, uint256 _value) {
+  function transfer(address _to, uint256 _value) { // recipient and value
+    // Check if sender has adequate balance and check for overflows
+    require(balanceOf[msg.sender] >= _value && balanceOf[_to] + _value >= balanceOf[_to]);
+
     // Add and subtract new balances
     balanceOf[msg.sender] -= _value;
     balanceOf[_to] += _value
